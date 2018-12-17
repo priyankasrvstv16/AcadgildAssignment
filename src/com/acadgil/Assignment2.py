53d8816285c7e138e7e02970d2e510914813ce22
Priyanka srivastava <priyankasrvstv16@gmail.com>
2018-12-18 00:30:48 +0530

    Take input from user first name and last name and print reverse

---
+++ src/com/acadgil/Assignment2.py
@@ -0,0 +1,14 @@
+
+#assigment2
+# Write a Python program to accept the user's first and last name and then getting them
+#printed in the the reverse order with a space between first name and last name.
+
+fs = input("what is your first Name : ")
+ls = input("What is your last Name : ")
+
+print(ls , end = ' ')
+print(fs)
+
+#what is your first Name : Priyanka
+#What is your last Name : Srivatava
+#Srivatava Priyanka
