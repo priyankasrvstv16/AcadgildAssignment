757fc083aebe2fb7dcd126c430f17af6f8389469
Priyanka srivastava <priyankasrvstv16@gmail.com>
2018-12-19 01:18:45 +0530

    Assignment Reverse and statement

---
+++ src/com/acadgil/Ass1.1q4.py
@@ -0,0 +1,5 @@
+
+print("WE, THE PEOPLE OF INDIA, ")
+print("      having solemnly resolved to constitute India into a SOVEREIGN, !")
+print("            SOCIALIST SECULAR DEMOCRATIC REPUBLIC ")
+print("             and to secure to all its citizens ")
---
+++ src/com/acadgil/Reverse2.3.py
@@ -0,0 +1,4 @@
+
+R = input("Take your word: ")
+
+print(R[::-1])
