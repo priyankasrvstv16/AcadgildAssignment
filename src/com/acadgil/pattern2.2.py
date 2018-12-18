d05657145ce9f6ca4db414ee7ae35c0958d6b1d7
Priyanka srivastava <priyankasrvstv16@gmail.com>
2018-12-18 16:20:14 +0530

    pattern of * assignment 2.2

---
+++ src/com/acadgil/pattern2.2.py
@@ -0,0 +1,20 @@
+
+
+for i in range(1,6):
+
+
+    for j in range(1,i+1):
+
+        print("*" , end= " ")
+
+    print()
+
+
+for i in range(5,1,-1):
+
+
+    for j in range(2,i+1):
+
+        print("*" , end= " ")
+
+    print()
